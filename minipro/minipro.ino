9f169621a03a3fe24c153f076698295f009aa9a2
saswata792 <saswatadas792@gmail.com>
2021-05-12 18:43:10 +0530

    updated

--- minipro/minipro.ino
+++ minipro/minipro.ino
@@ -12,10 +12,10 @@ IPAddress subnet(255,255,255,0);
 
 ESP8266WebServer server(80);
 
-uint8_t LED1pin = 2;
+uint8_t LED1pin = 15;
 bool LED1status = LOW;
 
-uint8_t LED2pin = 3;
+uint8_t LED2pin = 14;
 bool LED2status = LOW;
 
 void setup() {
@@ -87,32 +87,22 @@ void handle_NotFound(){
 
 String SendHTML(uint8_t led1stat,uint8_t led2stat){
   String ptr = "<!DOCTYPE html> <html>\n";
-  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr +="<title>LED Control</title>\n";
-  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
-  ptr +=".button {display: block;width: 80px;background-color: #1abc9c;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
-  ptr +=".button-on {background-color: #1abc9c;}\n";
-  ptr +=".button-on:active {background-color: #16a085;}\n";
-  ptr +=".button-off {background-color: #34495e;}\n";
-  ptr +=".button-off:active {background-color: #2c3e50;}\n";
-  ptr +="p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
-  ptr +="</style>\n";
-  ptr +="</head>\n";
-  ptr +="<body>\n";
-  ptr +="<h1>ESP8266 Web Server</h1>\n";
-  ptr +="<h3>Using Access Point(AP) Mode</h3>\n";
+  ptr+="<body>";
+  ptr +="<h1>Router Signup</h1>\n";
+  ptr +="<form>";
+  ptr +="<label for='rtusername'>RouterUsername</label>";
+  ptr +="<input id='rtusername'></input>";
+  ptr +="<label for='rtpassword'>RouterPassword</label>";
+  ptr +="<input id='rtpassword'></input>";
+  ptr +="</form>";
+  ptr +="<form>";
+  ptr +="<label for='username'>Username</label>";
+  ptr +="<input id='username'></input>";
+  ptr +="<label for='password'>Password</label>";
+  ptr +="<input id='password'></input>";
+  ptr +="</form>";
   
-   if(led1stat)
-  {ptr +="<p>LED1 Status: ON</p><a class=\"button button-off\" href=\"/led1off\">OFF</a>\n";}
-  else
-  {ptr +="<p>LED1 Status: OFF</p><a class=\"button button-on\" href=\"/led1on\">ON</a>\n";}
-
-  if(led2stat)
-  {ptr +="<p>LED2 Status: ON</p><a class=\"button button-off\" href=\"/led2off\">OFF</a>\n";}
-  else
-  {ptr +="<p>LED2 Status: OFF</p><a class=\"button button-on\" href=\"/led2on\">ON</a>\n";}
-
+   
   ptr +="</body>\n";
   ptr +="</html>\n";
   return ptr;
